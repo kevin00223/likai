12d2a12dc60c28c6c471a48516d733bca9aca564
李凯 <likai@likaidemacbook-pro.local>
2016-07-30 19:46:46 +0800

    新建

---
+++ asdas/asdas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  asdas
+//
+//  Created by 李凯 on 16/7/30.
+//  Copyright © 2016年 kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ asdas/asdas/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  asdas
+//
+//  Created by 李凯 on 16/7/30.
+//  Copyright © 2016年 kevin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
